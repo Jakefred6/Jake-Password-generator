afb9b029838ea71881172190a6afed1975fd3b13
Jake Marsee <jakemarsee@Jakes-MacBook-Air.local>
2023-09-10 18:53:58 -0600

    edited notes and other small things

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 // Function to generate a random password
 function generatePassword() {
-    // Define character sets
     var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
     var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numericChars = "0123456789";
     var specialChars = "!@#$%^&*()-_+=<>?";
   
-    // Initialize an empty character set for the password
     var passwordChars = "";
   
     // Prompt for password length
-    var passwordLength = parseInt(prompt("Enter password length (between 8 and 128 characters):"));
+    var passwordLength = parseInt(prompt("Please Enter Your password length (between 8 and 128 characters):"));
   
-    // Validate password length
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
       alert("Please enter a valid password length between 8 and 128 characters.");
-      return generatePassword(); // Restart the process
+      return generatePassword(); 
     }
   
     // Prompt for character types
-    var includeLowercase = confirm("Include lowercase characters?");
-    var includeUppercase = confirm("Include uppercase characters?");
-    var includeNumeric = confirm("Include numeric characters?");
-    var includeSpecial = confirm("Include special characters?");
+    var includeLowercase = confirm("Do you want to include lowercase characters?");
+    var includeUppercase = confirm("Do you want to include uppercase characters?");
+    var includeNumeric = confirm("Do you want to include numeric characters?");
+    var includeSpecial = confirm("Do you want to include special characters?");
   
     // Validate at least one character type is selected
     if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
       alert("At least one character type must be selected.");
-      return generatePassword(); // Restart the process
+      return generatePassword();
     }
   
-    // Build the character set based on selected criteria
+    // This will build the character set based on selected criteria
     if (includeLowercase) {
       passwordChars += lowercaseChars;
     }
@@ -44,7 +41,7 @@ function generatePassword() {
       passwordChars += specialChars;
     }
   
-    // Generate the password
+    // This will generate the password
     var password = "";
     for (var i = 0; i < passwordLength; i++) {
       var randomIndex = Math.floor(Math.random() * passwordChars.length);
@@ -54,7 +51,8 @@ function generatePassword() {
     return password;
   }
   
-  // Get references to the #generate element
+//   I am not sure how to add a "yes" or "no" button. As of now it is just "ok" and "cancel"
+
   var generateBtn = document.querySelector("#generate");
   
   // Write password to the #password input
@@ -65,6 +63,8 @@ function generatePassword() {
     passwordText.value = password;
   }
   
-  // Add event listener to generate button
+
   generateBtn.addEventListener("click", writePassword);
+
+
   
