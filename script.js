f079f90c36a3519e8f8af137b4022fd0124075ad
Jake Marsee <jakemarsee@Jakes-MacBook-Air.local>
2023-09-18 20:52:11 -0600

    made HTML easier to read

--- script.js
+++ script.js
@@ -9,24 +9,27 @@ function generatePassword() {
   
     // password length
     var passwordLength = parseInt(prompt("Please Enter Your password length (between 8 and 128 characters):"));
-  
+
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
       alert("Please enter a valid password length between 8 and 128 characters.");
       return generatePassword(); 
     }
   
+
     // character types
     var includeLowercase = confirm("Do you want to include lowercase characters?");
     var includeUppercase = confirm("Do you want to include uppercase characters?");
     var includeNumeric = confirm("Do you want to include numeric characters?");
     var includeSpecial = confirm("Do you want to include special characters?");
   
+
     //  at least one character type
     if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
       alert("At least one character type must be selected.");
       return generatePassword();
     }
   
+
     // This will build the character set based on selected criteria
     if (includeLowercase) {
       passwordChars += lowercaseChars;
@@ -41,17 +44,19 @@ function generatePassword() {
       passwordChars += specialChars;
     }
   
+
     // This will generate the password
     var password = "";
     for (var i = 0; i < passwordLength; i++) {
       var randomIndex = Math.floor(Math.random() * passwordChars.length);
       password += passwordChars.charAt(randomIndex);
     }
-  
-    return password;
+     return password;
   }
   
+
 //   I am not sure how to add a "yes" or "no" button. As of now it is just "ok" and "cancel"
+
 
   var generateBtn = document.querySelector("#generate");
   
@@ -63,7 +68,6 @@ function generatePassword() {
     passwordText.value = password;
   }
   
-
   generateBtn.addEventListener("click", writePassword);
 
 
